888516bbdbd9b14ce2d2f05768e17e900d7d77ea
egonlowe <elowe7@elon.edu>
2025-10-14 19:13:05 -0400

    implemented collidable interface, lot of small errors throughout

--- src/Paddle.java
+++ src/Paddle.java
@@ -4,59 +4,69 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-public class Paddle {
-	public static int PADDLE_WIDTH = 92; //placeholder until we run and see how everything fits
-	public static int PADDLE_HEIGHT = 12;
-	public static final int POWERED_UP_PADDLE = PADDLE_WIDTH * 2;
-	public static final int PADDLE_VELOCITY = 20; //placeholder until we decide on a set value
-	public static final String PADDLE_IMAGE = "resources/Paddle.png";
-	public static final int Y_COORDINATE = 628;
-	public static final int START_X = 220;
-	public ImageView myView;
- 
-	public Image paddleImage;
-	
-	public int xCoordinate; //placeholder 
-	
-	public Paddle (int posX, int posY) {
-		try {
-			this.paddleImage = new Image(new FileInputStream(PADDLE_IMAGE));
-		}
-		catch (FileNotFoundException e) {};
-		 myView = new ImageView(paddleImage);
-		 myView.setX(posX);
-		 myView.setY(posY);
-	}
-	
-	 public Node asNode() {
-	        return myView;
-	    }
-	
-	public void changeWidth(int newWidth) {
-		PADDLE_WIDTH = newWidth;
-	}
-	
-	public void handleKeyInput(KeyCode code) {
-		if(code == KeyCode.LEFT) {
-			xCoordinate+= PADDLE_VELOCITY;
-		} else if(code == KeyCode.RIGHT) {
-			xCoordinate-=PADDLE_VELOCITY;
-		}
-		
-	}
-	
-	
 
-	public void reset() {
-		xCoordinate = START_X;
-	}
-	
-	public void move(KeyCode code) {
-		if(code == KeyCode.LEFT) {
-			xCoordinate -= PADDLE_VELOCITY;
-		}
-		else if (code == KeyCode.RIGHT) {
-			xCoordinate += PADDLE_VELOCITY;
-		}
-	}
+/**
+ * Simple paddle that moves left/right and can be hit by the ball.
+ * Implements Collidable so the game can check collisions easily.
+ */
+public class Paddle implements Collidable {
+    public static int PADDLE_WIDTH = 92;
+    public static int PADDLE_HEIGHT = 12;
+    public static final int PADDLE_VELOCITY = 20;
+    public static final String PADDLE_IMAGE = "resources/Paddle.png";
+
+    private ImageView myView;
+    private int xCoordinate;
+
+    public Paddle(int startX, int startY) {
+        try {
+            Image img = new Image(new FileInputStream(PADDLE_IMAGE));
+            myView = new ImageView(img);
+        } catch (FileNotFoundException e) {
+            System.out.println("Paddle image not found!");
+            myView = new ImageView();
+        }
+
+        myView.setFitWidth(PADDLE_WIDTH);
+        myView.setFitHeight(PADDLE_HEIGHT);
+        myView.setX(startX);
+        myView.setY(startY);
+
+        xCoordinate = startX;
+    }
+
+    // getter for Game / collision checks
+    public Node asNode() {
+        return myView;
+    }
+
+    // move paddle based on key input
+    public void move(KeyCode code) {
+        if (code == KeyCode.LEFT) {
+            xCoordinate -= PADDLE_VELOCITY;
+        } else if (code == KeyCode.RIGHT) {
+            xCoordinate += PADDLE_VELOCITY;
+        }
+        // make sure paddle doesn't go off screen
+        if (xCoordinate < 0) xCoordinate = 0;
+        if (xCoordinate + PADDLE_WIDTH > 480) xCoordinate = 480 - PADDLE_WIDTH; // assuming screen width 480
+        myView.setX(xCoordinate);
+    }
+
+
+    @Override
+    public boolean checkCollision(Ball ball) {
+        return myView.getBoundsInParent().intersects(ball.getView().getBoundsInParent());
+    }
+
+    @Override
+    public void onCollision(Ball ball) {
+        // just make ball bounce up
+        ball.reverseY();
+    }
+
+    @Override
+    public Node getView() {
+        return myView;
+    }
 }
